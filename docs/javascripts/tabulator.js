e8681796e27ee9d86b46b31a51cd9cc5240b1100
Siroccomeister <137876722+Siroccomeister@users.noreply.github.com>
2024-09-24 09:35:53 +0200

    test tabulator

---
+++ docs/javascripts/tabulator.js
@@ -0,0 +1,152 @@
+//document$.subscribe(function() {
+
+//custom max min header filter
+var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParams){
+
+    var end;
+
+    var container = document.createElement("span");
+
+    //create and style inputs
+    var start = document.createElement("input");
+    start.setAttribute("type", "number");
+    start.setAttribute("placeholder", "Min");
+    start.setAttribute("min", 0);
+    start.setAttribute("max", 100);
+    start.style.padding = "4px";
+    start.style.width = "50%";
+    start.style.boxSizing = "border-box";
+
+    start.value = cell.getValue();
+
+    function buildValues(){
+        success({
+            start:start.value,
+            end:end.value,
+        });
+    }
+
+    function keypress(e){
+        if(e.keyCode == 13){
+            buildValues();
+        }
+
+        if(e.keyCode == 27){
+            cancel();
+        }
+    }
+
+    end = start.cloneNode();
+    end.setAttribute("placeholder", "Max");
+
+    start.addEventListener("change", buildValues);
+    start.addEventListener("blur", buildValues);
+    start.addEventListener("keydown", keypress);
+
+    end.addEventListener("change", buildValues);
+    end.addEventListener("blur", buildValues);
+    end.addEventListener("keydown", keypress);
+
+
+    container.appendChild(start);
+    container.appendChild(end);
+
+    return container;
+ }
+
+//custom max min filter function
+function minMaxFilterFunction(headerValue, rowValue, rowData, filterParams){
+    //headerValue - the value of the header filter element
+    //rowValue - the value of the column in this row
+    //rowData - the data for the row being filtered
+    //filterParams - params object passed to the headerFilterFuncParams property
+
+        if(rowValue){
+            if(headerValue.start != ""){
+                if(headerValue.end != ""){
+                    return rowValue >= headerValue.start && rowValue <= headerValue.end;
+                }else{
+                    return rowValue >= headerValue.start;
+                }
+            }else{
+                if(headerValue.end != ""){
+                    return rowValue <= headerValue.end;
+                }
+            }
+        }
+
+    return true; //must return a boolean, true if it passes the filter.
+}
+
+var table1 = new Tabulator("#example-table", {
+    ajaxURL:"../../assets/tables/scorecard.json",  
+    height:"311px",
+    layout:"fitColumns",
+    columns:[
+        {title:"Area", field:"area", width:100, editor:"input", headerFilter:"list", headerFilterParams:{valuesLookup:true, clearable:true}},
+        {title:"Discover & Assess", field:"disco", width:160, formatter:"progress", formatterParams:{
+    min:0,
+    max:1,
+    color:["#f36f76", "#f0a362", "#1cf28c"],
+    legendColor:"#000000",
+    legendAlign:"center",
+    legend : true
+    },
+headerFilter:"number", headerFilterPlaceholder:"at least...", headerFilterFunc:">="},
+        {title:"Design & Deploy", field:"build", width:160, formatter:"progress", formatterParams:{
+    min:0,
+    max:1,
+    color:["#f36f76", "#f0a362", "#1cf28c"],
+    legendColor:"#000000",
+    legendAlign:"center",
+    legend : true
+    },
+headerFilter:"number", headerFilterPlaceholder:"at least...", headerFilterFunc:">="},
+        {title:"Manage & Change", field:"run", width:160, formatter:"progress", formatterParams:{
+    min:0,
+    max:1,
+    color:["#f36f76", "#f0a362", "#1cf28c"],
+    legendColor:"#000000",
+    legendAlign:"center",
+    legend : true
+    },
+headerFilter:"number", headerFilterPlaceholder:"at least...", headerFilterFunc:">="},
+        {title:"Expert @Teams", field:"email", hozAlign:"center", formatter:"link", formatterParams:{
+    labelField:"name",
+    urlPrefix:"msteams:/l/chat/0/0?users=",
+    target:"_blank"},
+    headerFilter:"list",
+    headerFilterParams:{valuesLookup:true, clearable:true}
+    },
+    {title:"Role & City", field:"role - city", editor:"input", headerFilter:"list", headerFilterParams:{valuesLookup:true, clearable:true}},
+],
+});
+
+//table1.on("tableBuilt", () => {
+//  table1.setPage(2);
+//});
+
+var table2 = new Tabulator("#checkerboard-table", {
+    ajaxURL:"./assets/tables/checkerboard.json",
+    // csv format not workable at this moment - keep checking tabulator.info website
+    height:"669px",
+    layout:"fitColumns",
+    groupBy:"customer",
+    columns:[
+        // 2 first columns are frozen     
+        {title:"Customer Journey", field:"blocks", width:280, hozAlign:"right", frozen:true},
+        {title:"Solution Areas", hozAlign:"center",
+        columns:[
+                {title:"Operational Efficiency", headerTooltip:true, field:"efficiency", formatter:"progress",formatterParams:{min:0,max:1,color:"#1cf28c"}},
+                {title:"Connected Smart Products", headerTooltip:true, field:"smart", formatter:"progress",formatterParams:{min:0,max:1,color:"#1cf28c"}},
+                {title:"Secured Flexible Infrastructure", headerTooltip:true, field:"infra", formatter:"progress",formatterParams:{min:0,max:1,color:"#1cf28c"}},
+                {title:"ESG", headerTooltip:true, field:"esg", formatter:"progress",formatterParams:{min:0,max:1,color:"#1cf28c"}},
+                {title:"Assisted Worker", headerTooltip:true, field:"worker", formatter:"progress",formatterParams:{min:0,max:1,color:"#1cf28c"}},    
+        ]
+            }
+            ],
+});
+
+table.on("tableBuilt", () => {
+  table.setPage(2);
+});
--- docs/stats.md
+++ docs/stats.md
@@ -11,5 +11,7 @@ TOTAL KM from BANFF to ANTELOPE WELLS/Mexican Border : 4'411 km.
 TOTAL KM from CALGARY to ANTELOPE WELLS/Mexican Border : 4'581 km.
 On average I rode at 16.6 km/h with 8 hours/day.
 
+<div id="example-table"></div>
+
 {{ read_csv('assets/tables/GDMBR.csv') }}
 
